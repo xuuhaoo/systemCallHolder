cb2abe076952e1b8bcbcc3f969c2d873efcac73e
徐昊 <xuhao@mucang.cn>
2023-03-01 02:55:05 +0800

    提交下代码

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -36,24 +36,26 @@ void childProcess() {
     int status;
     long no;
     long addr;
+    LogI("childProcess prepare attach father process");
     ptrace(PTRACE_ATTACH, getppid(), NULL, NULL);
+    waitpid(getppid(), &status, 0);
+    LogI("childProcess have attached to father process");
     ptrace(PTRACE_SYSCALL, getppid(), NULL, NULL);
     while (true) {
         // 等待主进程的系统调用
-        waitpid(getppid(), &status, WUNTRACED);
-
+        waitpid(getppid(), &status, 0);
         if (WIFEXITED(status)) {
-            LogI("wtatus exited:%d", status);
+            LogI("childProcess exited:%d", status);
             break;
         }
         // 获取系统调用号
         no = GetSysCallNo(getppid());
+//        LogI("childProcess wait status sig %ld",no);
         // 如果是svc调用，获取入参和返回值
         if (no == __NR_openat) {
-            LogI("syscall: %ld", no);
+            LogI("childProcess found syscall: %ld", no);
             addr = GetOpenAtFileNameAddr(getppid());
-            LogI("fileNameAddr: %p ,%ld", (void *) addr, addr);
-
+            LogI("childProcess get file name addr:%p ,addr in long:%ld", (void *) addr, addr);
             // 读取字符串
             int i = 0;
             char buf[4096];
@@ -61,7 +63,7 @@ void childProcess() {
                 long val = ptrace(PTRACE_PEEKTEXT, getppid(), (void *) addr, NULL);
                 if (val == -1) {
                     // 出现错误
-                    LogI("openat -1 syscall: %ld data:%s", no, buf);
+                    LogI("childProcess read openat err:-1 data:%s", buf);
                     break;
                 }
                 addr += sizeof(long);
@@ -69,12 +71,16 @@ void childProcess() {
                 if (memchr(&val, 0, sizeof(val))) break;
                 i += sizeof(long);
             }
-            LogI("openat syscall: %ld data:%s", no, buf);
+            LogI("childProcess read openat data:%s", buf);
+
+        } else {
+            LogI("childProcess found next");
+            ptrace(PTRACE_CONT, getppid(), 0, 0);
         }
         ptrace(PTRACE_SYSCALL, getppid(), NULL, NULL);
     }
     ptrace(PTRACE_DETACH, getppid(), NULL, NULL);
-    LogI("detach...");
+    LogI("childProcess detach father process...");
 }
 
 
@@ -82,7 +88,7 @@ extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_squareup_systemcall_Native_readFileSysCall(JNIEnv *env, jobject thiz, jstring filePath) {
     char *path = jstringToChar(env, filePath);
-    LogI("path addr:%p", path);
+    LogI("read file syscall is called and path addr:%p", path);
     long fd = syscall(__NR_openat, AT_FDCWD, path, O_RDONLY);
     char buf[100];
     std::string str;
@@ -92,15 +98,17 @@ Java_com_squareup_systemcall_Native_readFileSysCall(JNIEnv *env, jobject thiz, j
     }
     syscall(__NR_close, fd);
     return env->NewStringUTF(str.c_str());
-
 }
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_squareup_systemcall_Native_ptraceViewSvcCall(JNIEnv *env, jobject thiz) {
     pid_t childId = fork();
     if (childId == 0) {//子进程
+        LogI("child process is on");
         childProcess();
     } else {//父进程
+        LogI("father process waiting for %d attach ", childId);
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
     }
 }
